e0a16bab2dcbd01a5ac57e309a490b155c9a6847
7thDragon <slava.lepeshkin01@gmail.com>
2023-03-06 00:20:47 +0600

    feat: add page buttons

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,22 +1,39 @@
+import { observer } from "mobx-react-lite";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 import Clock from "../../components/Clock/clock";
-import DropFilter from "../../components/FilterButton/DropFilter/dropFilter";
-import FrodFilter from "../../components/FilterButton/FrodFilter/frodFilter";
+import DropFilter from "../../components/Buttons/DropFilter/dropFilter";
+import FrodFilter from "../../components/Buttons/FrodFilter/frodFilter";
 import ModalWindow from "../../components/ModalWindow/modalWindow";
+import Pagination from "../../components/Pagination/pagination";
 import TransactionsTable from "../../components/TransactionsTable/transactionsTable"
+import transactionsStore from "../../strore/transactionsStore";
 import style from "./home.module.css"
+import StatisticB from "../../components/Buttons/StatisticB/statisticB";
 
-const Home = () => {
+const Home = observer(() => {
+    const { page } = useParams();
+    if(page) {
+        const startTransaction = (Number(page) - 1) * transactionsStore.transactionsLimit + 1
+        transactionsStore.setStartTransaction(startTransaction);
+    useEffect(() => {
+            transactionsStore.getTransactions();
+      }, [page]);
+    }
+    
     return (
         <div className={style.main_block}>
             <div className={style.buttons_block}>
-                <FrodFilter />
                 <Clock />
+                <StatisticB />
+                <FrodFilter />
                 <DropFilter />
             </div>
             <TransactionsTable />
             < ModalWindow />
+            <Pagination maxPages={transactionsStore.pageLimit} />
         </div>
     );
-}
+})
 
 export default Home;
