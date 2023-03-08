ed68646113105eaf309d16c0c1d6d6af73506c67
7thDragon <slava.lepeshkin01@gmail.com>
2023-03-09 01:58:35 +0600

    feat: add fraud filter

--- src/components/Buttons/DropFilter/dropFilter.tsx
+++ src/components/Buttons/DropFilter/dropFilter.tsx
@@ -1,10 +1,21 @@
-import transactionsStore from "../../../strore/transactionsStore";
 import style from '../buttons.module.css'
 import { BiReset } from 'react-icons/bi'
+import { useNavigate, useParams } from "react-router-dom";
+import transactionsStore from '../../../strore/transactionsStore';
 
 const DropFilter = () => {
+    const { limit, page } = useParams();
+    const lim = Number(limit);
+    const active = Number(page);
+    const navigation = useNavigate();
+    const paginationNavigate = (fraud: number, num: number, lim: number) => {
+        navigation({
+            pathname: `../fraud/${fraud}/limit/${lim}/page/${num}`
+        });
+        transactionsStore.dropFilters();
+};
     return (
-        <button onClick={() => transactionsStore.dropFilters()} className={style.buttons}>Сброс <BiReset style={{fontSize:20}}/></button>
+        <button onClick={() => paginationNavigate(0, active, lim)} className={style.buttons}>Сброс <BiReset style={{fontSize:20}}/></button>
     );
 }
 
--- src/components/Buttons/FrodFilter/frodFilter.tsx
+++ src/components/Buttons/FrodFilter/frodFilter.tsx
@@ -1,10 +1,18 @@
 import style from '../buttons.module.css'
 import {BsSearch} from 'react-icons/bs'
-import transactionsStore from '../../../strore/transactionsStore'
+import { useNavigate, useParams } from 'react-router-dom';
 
 const FrodFilter = () => {
+    const { limit} = useParams();
+    const lim = Number(limit);
+    const navigation = useNavigate();
+    const paginationNavigate = (fraud: number, num: number, lim: number) => {
+        navigation({
+            pathname: `../fraud/${fraud}/limit/${lim}/page/${num}`
+        });
+};
     return (
-        <button onClick={() =>transactionsStore.frodOnly()} className={style.buttons}>Только подозрительные <BsSearch /></button>
+        <button onClick={() =>{paginationNavigate(1, 1, lim)}} className={style.buttons}>Только подозрительные <BsSearch /></button>
     )
 }
 
